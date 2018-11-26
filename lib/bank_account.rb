f18df22e6c7a44bdfa04fb7ef734abf134fa33f4
Johnathon Wood <jaywood128@gmail.com>
2018-11-26 15:59:41 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -20,10 +20,11 @@ class BankAccount
   end
 
   def valid?
-  if  self.balance > 0 && self.status = "open"
-    true
-  else
-    false
+    if  self.balance > 0 && self.status = "open"
+      true
+    else
+      false
+  end
 
 
 end
